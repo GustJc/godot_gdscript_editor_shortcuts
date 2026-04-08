dc9d1ffbe780367feef73f3ff7ef62229d885662
Gustavo Jaruga Cruz <gustavojarugacruz@gmail.com>
2026-04-08 19:21:39 -0300

    Add colon & new blank line shortcut

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Gdscript Space Block Jumper
 
-Very simple plugin that let's you move around your code really fast with shortcuts.
+Very simple plugin that let's you move around your code really fast with extra code shortcuts.
 
 ![](example_jumper.gif)
 
@@ -16,6 +16,11 @@ Shortcuts:
 - Shift + Page Down : Go to next 'func'
 - Shift + Page Up : Go to previous 'func'
 
+- Ctrl + Enter : Add (:) colon at end and create new line below
+
+Tip: Change the editor shortcut of `ui_text_newline_blank` to something else like `alt + enter` to create the newline without the colon.
+     This addon also fixes the autocomplete hint not disappearing when creating a new blank line.
+
 # Configuration
 
 You can set the `Shift to Move Space Behavior` configuration in the ProjectSettings/Plugins/Gdscript Block Jumper.
@@ -27,5 +32,5 @@ PageUp/Down will scroll to func and shift+PageUp/Down will jump to empty lines
 ## Todo
 
 - Add ability to select lines while jumping around
-- Add configurable hotkeys
+- Add configurable hotkeys (note: careful because of Godot bug in project setting resources)
 - Use some option+arrow combination to move, if configurable hotkeys doesn't work for that.
--- addons/gdscript_block_jumper/plugin_block_jumper.gd
+++ addons/gdscript_block_jumper/plugin_block_jumper.gd
@@ -54,6 +54,58 @@ func _input(event: InputEvent) -> void:
 					normal_action.call(code_edit)
 				get_viewport().set_input_as_handled()
 
+		# Fixes the removal of autocomplete on new blank lines
+		if event.is_action_pressed("ui_text_newline_blank", true):
+			var code_edit: CodeEdit = EditorInterface.get_script_editor().get_current_editor().get_base_editor()
+			code_edit.cancel_code_completion()
+			code_edit.set_code_hint("")
+
+		# Add colon endline and create newline
+		if event.keycode == KEY_ENTER and event.pressed:
+			var code_edit: CodeEdit = EditorInterface.get_script_editor().get_current_editor().get_base_editor()
+			if code_edit.has_focus():
+				var this_ev : InputEventKey = InputEventKey.new()
+				this_ev.keycode = KEY_ENTER
+				this_ev.ctrl_pressed = true
+				#this_ev.alt_pressed = true
+				# Enter + Ctrl + Alt (not shift)
+				if event.is_match(this_ev):
+					add_colon_jump_line(code_edit)
+
+					var blank_line_ev := InputMap.action_get_events("ui_text_newline_blank")[0]
+					if not this_ev.is_match(blank_line_ev):
+						execute_new_blank_line_shortcut()
+						# Consume this input, since calling input key for newline
+						get_viewport().set_input_as_handled()
+
+
+#  Manually trigger the 'newline_blank' action
+# # Did not work with InputAction #ev.action = "ui_text_newline_blank"
+# # Needs manual InputEventKey
+func execute_new_blank_line_shortcut() -> void:
+	var ev = InputEventKey.new()
+	ev = InputMap.action_get_events("ui_text_newline_blank")[0].duplicate()
+	ev.pressed = true
+
+	Input.parse_input_event(ev)
+	ev = ev.duplicate()
+	ev.pressed = false
+	Input.parse_input_event(ev)
+
+func add_colon_jump_line(code_edit: CodeEdit) -> void:
+	var caret_line = code_edit.get_caret_line()
+	var column = code_edit.get_caret_column()
+
+	var line_text = code_edit.get_line(caret_line)
+
+	# Check if not empty and if it has colon
+	if not line_text.strip_edges().ends_with(":"):
+		var updated_text = line_text + ":"
+		code_edit.set_line(caret_line, updated_text)
+		# Set cursor to the end, so newline action can do its job
+		code_edit.set_caret_column(updated_text.length())
+	return
+
 
 func move_prev_function(code_edit: CodeEdit) -> void:
 	var caret_line = code_edit.get_caret_line()
